9bcb5f4dec2f10e69a3bba19ae3a0f7f996124ea
wossshts <47068247+jzzzj@users.noreply.github.com>
2019-06-10 18:52:19 -0400

    Update one.php

--- one.php
+++ one.php
@@ -6,18 +6,12 @@
 <body>
   <p>Helo world</p>
 <?php
-if (isset($_REQUEST['email']))
-//if "email" is filled out, send email
-  {
-  //send email
-  $email = $_REQUEST['email'] ; 
-  $subject = $_REQUEST['subject'] ;
-  $message = $_REQUEST['message'] ;
+  $email = $_GET['email'] ; 
+  $subject = $_GET['subject'] ;
+  $message = $_GET['message'] ;
   mail( "jsisbetter@gmail.com", "Subject: $subject",
   $message, "From: $email" );
   echo "Thank you for using our mail form";
-  }
-else
 ?>
 </body>
 </html>
