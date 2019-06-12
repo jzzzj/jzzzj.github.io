34705f0fd501c014f3184ce35b9c57b8d84a81f1
wossshts <47068247+jzzzj@users.noreply.github.com>
2019-06-11 20:53:28 -0400

    Update one.php

--- one.php
+++ one.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>SITS</title>
-</head>
-<body>
-  <p>Helo world</p>
+
 <?php
   $email = $_POST['email'] ; 
   $subject = $_POST['subject'] ;
   $message = $_POST['message'] ;
-  mail( "jsisbetter@gmail.com", "Subject: $subject", $message, "From: $email" );
-  echo "Thank you for using our mail form";
+  echo $email;
+  echo $subject;
+  echo $message;
 ?>
-</body>
-</html>
+
